2b4a88845e6fa854d39088b8c93562f86baf5bd0
Greg Noethlich <gmnoethlich@gmail.com>
2023-10-18 17:29:59 -0500

    drawing, styling, positioning, text

--- script.js
+++ script.js
@@ -1,76 +1,34 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-let particleArray = [];
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-// handle mouse
-const mouse = {
-    x: null,
-    y: null,
-    radius: 150
-}
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'red';
+    ctx.beginPath();
+    ctx.moveTo(canvas.width/2, 0);
+    ctx.lineTo(canvas.width/2, canvas.height);
+    ctx.stroke();
 
-window.addEventListener('mousemove', function(e) {
-    mouse.x = e.x;
-    mouse.y = e.y;
-    // console.log(mouse.x, mouse.y);
+    ctx.strokeStyle = 'green';
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height/2);
+    ctx.lineTo(canvas.width, canvas.height/2);
+    ctx.stroke();
+    
+
+    const text = 'Hello!';
+    const textX = canvas.width/2;
+    const textY = canvas.height/2;
+    console.log(ctx);
+    ctx.fillStyle='blue';
+    ctx.strokeStyle = 'cyan';
+
+    ctx.font = '80px Helvetica';
+    ctx.textAlign='center';
+    
+    ctx.letterSpacing = '20px';
+    ctx.fillText(text, textX, textY);
+    ctx.strokeText(text, textX, textY);
 });
-
-ctx.fillStyle = 'white';
-ctx.font = '30px Verdana';
-ctx.fillText('A', 0, 30);
-// ctx.strokeStyle = 'white';
-// ctx.strokeRect(0, 0, 100, 100);
-const data = ctx.getImageData(0, 0, 100, 100);
-
-class Particle {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.size = 3;
-        this.baseX = this.x;
-        this.baseY = this.y;
-        this.density = (Math.random() * 30) + 1;
-    }
-    draw() {
-        ctx.fillStyle = 'cyan';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fill();
-    }
-    update() {
-        let dx = mouse.x - this.x;
-        let dy = mouse.y - this.y;
-        let distance = Math.sqrt(dx * dx + dy * dy);
-        if (distance < 100) {
-            this.size = 7;
-        } else {
-            this.size = 3;
-        }
-    }
-}
-
-function init() {
-    particleArray = [];
-    for (let i = 0; i < 1000; i++) {
-        let x = Math.random() * canvas.width;
-        let y = Math.random() * canvas.height;
-        particleArray.push(new Particle(x, y));
-    }
-    // particleArray.push(new Particle(200, 200));
-    // particleArray.push(new Particle(300, 300));
-}
-init();
-console.log(particleArray);
-
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < particleArray.length; i++) {
-        particleArray[i].draw();
-        particleArray[i].update();
-    }
-    requestAnimationFrame(animate);
-}
-animate();
