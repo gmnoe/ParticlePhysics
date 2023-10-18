ee5d28e402a32f8df79cda02d073985bf38bb743
Greg Noethlich <gmnoethlich@gmail.com>
2023-10-18 18:31:46 -0500

    Multiline centered text

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ window.addEventListener('load', function() {
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    console.log(ctx);
 
+    // Drawing the axis y
     ctx.lineWidth = 3;
     ctx.strokeStyle = 'red';
     ctx.beginPath();
@@ -11,24 +13,43 @@ window.addEventListener('load', function() {
     ctx.lineTo(canvas.width/2, canvas.height);
     ctx.stroke();
 
+    // Drawing the axis x
     ctx.strokeStyle = 'green';
     ctx.beginPath();
     ctx.moveTo(0, canvas.height/2);
     ctx.lineTo(canvas.width, canvas.height/2);
     ctx.stroke();
     
-
-    const text = 'Hello!';
-    const textX = canvas.width/2;
-    const textY = canvas.height/2;
-    console.log(ctx);
     ctx.fillStyle='blue';
     ctx.strokeStyle = 'cyan';
-
     ctx.font = '80px Helvetica';
     ctx.textAlign='center';
-    
-    ctx.letterSpacing = '20px';
-    ctx.fillText(text, textX, textY);
-    ctx.strokeText(text, textX, textY);
+    ctx.textBaseline = 'middle';
+
+    const maxTextWidth = canvas.width * 1;
+    const lineHeight = 80;
+
+    function wrapText(text) {
+        let linesArray = [];
+        let lineCounter = 0;
+        let line = '';
+        let words = text.split(' ');
+        for (let i = 0; i < words.length; i++) {
+            let testLine = line + words[i] + ' ';
+            if (ctx.measureText(testLine).width > maxTextWidth) {
+                line = words[i] + ' ';
+                lineCounter++;
+            } else {
+                line = testLine;
+            }
+            linesArray[lineCounter] = line;
+        }
+        let textHeight = lineHeight * lineCounter;
+        let textY = canvas.height/2 - textHeight/2;
+        linesArray.forEach((el, index) => {
+            ctx.fillText(el, canvas.width/2, textY + (index * lineHeight));
+        });
+        console.log(linesArray)
+    }
+    wrapText('Hello, how are you? Testing testing testing');
 });
