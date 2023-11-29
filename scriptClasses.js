70ba4a4737109b4a03658eff3b030c9e80897bd2
Greg Noethlich <gmnoethlich@gmail.com>
2023-11-29 10:58:01 -0600

    Scanning the imagedate for pixels

--- scriptClasses.js
+++ scriptClasses.js
@@ -24,8 +24,8 @@ window.addEventListener('load', function() {
             this.canvasHeight = canvasHeight;
             this.textX = this.canvasWidth / 2;
             this.textY = this.canvasHeight / 2;
-            this.fontSize = 100;
-            this.lineHeight = this.fontSize * 0.8;
+            this.fontSize = 80;
+            this.lineHeight = this.fontSize * 0.9;
             this.maxTextWidth = this.canvasWidth * 0.8;
             this.textInput = document.getElementById('textInput');
             this.textInput.addEventListener('keyup', (e) => {
@@ -33,6 +33,18 @@ window.addEventListener('load', function() {
                     this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
                     this.wrapText(e.target.value);
                 }
+            });
+            // particle text
+            this.particles = [];
+            this.gap = 3;
+            this.mouse = {
+                radius: 20000,
+                x: 0,
+                y: 0
+            }
+            window.addEventListener('mousemove', (e) => {
+                this.mouse.x = e.x;
+                this.mouse.y = e.y;            
             });
         }
         wrapText(text) {
@@ -68,9 +80,23 @@ window.addEventListener('load', function() {
                 this.context.fillText(el, this.textX, this.textY + (index * this.lineHeight));
                 this.context.strokeText(el, this.textX, this.textY + (index * this.lineHeight));
             });
+            this.convertToParticles();
         }
         convertToParticles() {
-
+            this.particles = [];
+            const pixels = this.context.getImageData(0, 0, this.canvasWidth, this.canvasHeight).data;
+            for (let y = 0; y < this.canvasHeight; y+=this.gap) {
+                for (let x = 0; x < this.canvasWidth; x+= this.gap) {
+                    const index = (y * this.canvasWidth + x) * 4;
+                    const alpha = pixels[index + 3];
+                    if (alpha > 0) {
+                        const red = pixels[index];
+                        const green = pixels[index + 1];
+                        const blue = pixels[index + 2];
+                        const color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+                    }
+                }
+            }
         }
         render() {
 
@@ -79,7 +105,6 @@ window.addEventListener('load', function() {
 
     const effect = new Effect(ctx, canvas.width, canvas.height);
     effect.wrapText('Hello, how are you today?');
-    console.log(effect);
 
     function animate() {
 
